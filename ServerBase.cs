3bb5368fda7a866d31a052a810cd36c65d6d5564
CULRRY <rkaghwns8@naver.com>
2024-10-12 00:48:48 +0900

    초기 커밋.

---
+++ ServerBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTServerLib
+{
+    abstract class ServerBase(ushort port)
+    {
+        public ushort Port { get; } = port;
+
+        public bool SendPacket()
+        {
+            return true;
+        }
+
+        public bool Disconnect()
+        {
+            return true;
+        }
+
+        protected abstract void OnRecv();
+        protected abstract void OnAccept();
+        protected abstract void OnDisconnect();
+
+    }
+}
